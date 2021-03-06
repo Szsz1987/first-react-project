9029ceb158455a3ccb902e878c45d3de2523da28
Szymon <sszulinski@gmail.com>
2021-03-06 13:58:52 +0100

    add new component - header (navigation)

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Header from '../Header/Header';
 
 const MainLayout = ({children}) => (
   <div>
+    <Header />
     {children}
   </div>
 );
@@ -10,5 +12,7 @@ const MainLayout = ({children}) => (
 MainLayout.propTypes = {
   children: PropTypes.node,
 };
+// Komponent Header wyświetli nawigację, podczas gdy props {children} będzie renderować wszystko, 
+// co zostało dodane w tagach <MainLayout> w App.js.
 
 export default MainLayout;
